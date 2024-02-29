bb3dfc0aae2e68ee7bb372613718c76b88763702
Amit Karsale <amit.karsale@perforce.com>
2024-02-29 18:49:26 +0530

    (PA-6001) Add sles11 platform to pxp-agent

--- configs/platforms/sles-11-x86_64.rb
+++ configs/platforms/sles-11-x86_64.rb
@@ -3,8 +3,8 @@ platform "sles-11-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  # plat.add_build_repository "http://osmirror.delivery.puppetlabs.net/sles-11-deps-x86_64/sles-11-deps-x86_64.repo"
-  # plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
+  plat.add_build_repository "http://osmirror.delivery.puppetlabs.net/sles-11-deps-x86_64/sles-11-deps-x86_64.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
   packages = %w[
     aaa_base
     libbz2-devel
@@ -18,6 +18,5 @@ platform "sles-11-x86_64" do |plat|
   ]
   plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
   plat.provision_with("zypper -n --no-gpg-checks install -y #{packages.join(' ')}")
-  # plat.provision_with "zypper install -y --oldpackage pl-cmake-3.2.3-13.sles11.x86_64"
   plat.vmpooler_template "sles-11-x86_64"
 end
